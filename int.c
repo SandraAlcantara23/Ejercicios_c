e0d5808b0bba89ee884b2ccb63bba80cad7d9b45
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-10 14:20:04 -0600

    Add files via upload

---
+++ int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int c = 5;
+    printf ("%d", c);
+    printf ("%d", c++);
+    printf ("%d", c);
+
+    int d = 10;
+    printf ("%d", d);
+    printf ("%d", --d);
+    printf ("%d", d);
+
+}
